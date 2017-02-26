eab4cabc546b7dfc2f7a0eb2f73dc0108c5c3aea
kateGlebova <kate.glebova97@gmail.com>
2017-02-26 23:46:56 +0300

    Minor changes to lab1

--- lab1/js/sidebar.js
+++ lab1/js/sidebar.js
@@ -1,4 +1,4 @@
 var min = 1;
 var max = 4;
 var img_src = 'img/ads/' + Math.floor(Math.random() * (max - min) + min) + '.jpg';
-document.getElementById('side').innerHTML = '<img src=' + img_src + '/>';
+document.getElementById('side').innerHTML = "<img src='" + img_src + "'>";
