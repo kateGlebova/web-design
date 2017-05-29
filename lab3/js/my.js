c65b888e4752bb2f63f1ad0b6d1816882b425aed
kateGlebova <kate.glebova97@gmail.com>
2017-05-29 20:48:15 +0300

    Write HTML

---
+++ lab3/js/my.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	
+	// $('#edit-form').on('submit', function(event) {
+	// 	event.preventDefault();
+	// 	console.log($('#edit-form').serialize());
+	// })
+});
+
+function f() {
+		console.log($('#edit-form').serialize());
+	};
